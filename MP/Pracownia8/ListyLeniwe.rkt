76a7e5d2806a61a1f18a37a7356d398ff0c04eb1
unknown <dawidovsky22@gmail.com>
2018-04-17 20:38:12 +0200

    Pracownia 8 - Przed zajeciami

---
+++ MP/Pracownia8/ListyLeniwe.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+(define (lcons x f)
+  (cons x f))
+
+(define (lhead l)
+  (car l))
+
+(define (ltail l)
+  ((cdr l)))
+
+(define (nats-from m)
+  (lcons
+   m
+   (lambda () (nats-from (+ m 1)))))
+
+(define nats
+  (nats-from 0))
+
+(define (prime? n)
+  (define (div-by m)
+    (cond [(= m n) true]
+          [(= (modulo n m) 0) false]
+          [else (div-by (+ m 1))]))
+  (if (< n 2)
+      false
+      (div-by 2)))
+
+(define (fib-from m n)
+  (lcons
+   m
+   (lambda () (fib-from n (+ m n)))))
+
+(define fib
+  (fib-from 1 1))
+
+(define (take n l)
+  (if (or (null? l) (= n 0))
+      null
+      (cons (lhead l)
+            (take (- n 1) (ltail l)))))
+
+(define (filter p l)
+  (cond [(null? l) null]
+        [(p (lhead l))
+         (lcons (lhead l)
+                (lambda ()
+                  (filter p (ltail l))))]
+        [else (filter p (ltail l))]))
+
+
+(define (int-from m)
+  (lcons
+   m
+   (if (>= m 0)
+       (lambda () (int-from (- (+ m 1))))
+       (lambda () (int-from (- m))))))
+
+(define int
+  (int-from 0))
+
