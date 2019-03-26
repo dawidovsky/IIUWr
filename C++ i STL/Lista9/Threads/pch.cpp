662ba5b953eb096ca0449d79a489f64eaa9823b8
unknown <dawidovsky22@gmail.com>
2019-03-26 21:49:28 +0100

    Update 26.03

--- C++ i STL/Lista9/Threads/pch.cpp
+++
@@ -1,5 +0,0 @@
-﻿// pch.cpp: plik źródłowy odpowiadający wstępnie skompilowanemu nagłówkowi, niezbędny do powodzenia kompilacji
-
-#include "pch.h"
-
-// Ogólnie rzecz biorąc, zignoruj ten plik, ale miej go pod ręką, jeśli używasz wstępnie skompilowanych nagłówków.
